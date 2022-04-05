a3db25c3b835fff7341b2ba0fc0f68773027c59b
togo-mentor <uint2435@gmail.com>
2022-04-06 06:35:19 +0900

    signup, signin button

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
 import './App.css';
+import { useAuth0 } from "@auth0/auth0-react";
 
 function App() {
+  const { isAuthenticated,loginWithRedirect,logout } = useAuth0(); // 必要な機能をインポート
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
-      </header>
+      <div style={{padding:'20px'}}>
+        <h2>ログインボタン</h2>
+          <button onClick={() => loginWithRedirect()}>ログイン</button>
+        <h2>ログアウトボタン</h2>
+          <button onClick={() => logout()}>ログアウト</button>
+        <h2>ログイン状態</h2>
+        {
+          isAuthenticated ?
+          <p>ログイン</p>
+          :
+          <p>ログアウト</p>
+        }
+      </div>
     </div>
   );
 }
